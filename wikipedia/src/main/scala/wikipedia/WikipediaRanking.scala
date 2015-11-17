ede8ecb84f3ffb21093b1fd71342fdee11704e88
Florian Rossier <florian.rossier@gmail.com>
2015-11-17 10:40:14 +0100

    add sparkconf and spark context and create rdd from wikipedia

--- wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -3,8 +3,8 @@ package wikipedia
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-
 import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkConf
 
 case class WikipediaArticle(title: String, text: String)
 
@@ -14,11 +14,14 @@ object WikipediaRanking {
     "JavaScript", "Java", "PHP", "Python", "C#", "C++", "Ruby", "CSS",
     "Objective-C", "Perl", "Scala", "Haskell", "MATLAB", "Clojure", "Groovy")
 
-  val conf: SparkConf = ???
-  val sc: SparkContext = ???
-  val wikiRdd: RDD[WikipediaArticle] = ???
+  val conf: SparkConf = new SparkConf();
+  conf.setMaster("local[4]");
+  val sc: SparkContext = new SparkContext();
+  
+  val wikiRdd: RDD[WikipediaArticle] = sc.parallelize(WikipediaData.articles);
 
-  /** Returns the number of articles on which the language `lang` occurs.
+  /**
+   * Returns the number of articles on which the language `lang` occurs.
    *  Hint1: consider using method `aggregate` on RDD[T].
    *  Hint2: should you count the "Java" language when you see "JavaScript"?
    *  Hint3: the only whitespaces are blanks " "
@@ -62,19 +65,19 @@ object WikipediaRanking {
 
     /* Languages ranked according to (1) */
     val langsRanked: List[(String, Int)] = timed("Part 1: naive ranking", rankLangs(langs, wikiRdd))
-	println(langsRanked)
-	
+    println(langsRanked)
+
     /* An inverted index mapping languages to wikipedia pages on which they appear */
     def index: RDD[(String, Iterable[WikipediaArticle])] = makeIndex(langs, wikiRdd)
 
     /* Languages ranked according to (2), using the inverted index */
     val langsRanked2: List[(String, Int)] = timed("Part 2: ranking using inverted index", rankLangsUsingIndex(index))
-	println(langsRanked2)
-	
+    println(langsRanked2)
+
     /* Languages ranked according to (3) */
     val langsRanked3: List[(String, Int)] = timed("Part 3: ranking using reduceByKey", rankLangsReduceByKey(langs, wikiRdd))
-	println(langsRanked3)
-	
+    println(langsRanked3)
+
     /* Output the speed of each ranking */
     println(timing)
   }
